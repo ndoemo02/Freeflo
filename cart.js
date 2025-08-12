df314f0f8bd6d022991d01b3f5b8cf1ae18299f3
ndoemo02 <ndoemo02@gmail.com>
2025-08-12 03:01:20 +0200

    Update cart.js

--- cart.js
+++ cart.js
@@ -5,22 +5,14 @@ export function getCart() {
   try { return JSON.parse(localStorage.getItem(CART_KEY)) ?? []; }
   catch { return []; }
 }
-
 export function saveCart(items) {
   localStorage.setItem(CART_KEY, JSON.stringify(items));
   window.dispatchEvent(new CustomEvent('cart:update', { detail: items }));
 }
-
 export function addToCart(item) {
   const cart = getCart();
   cart.push({ id: crypto.randomUUID(), qty: 1, ...item });
   saveCart(cart);
 }
-
-export function removeFromCart(id) {
-  saveCart(getCart().filter(i => i.id !== id));
-}
-
-export function clearCart() { saveCart([]); }
-
-export function cartCount() { return getCart().reduce((n,i)=>n+(i.qty||1),0); }
+export function clearCart(){ saveCart([]); }
+export function cartCount(){ return getCart().reduce((n,i)=>n+(i.qty||1),0); }
